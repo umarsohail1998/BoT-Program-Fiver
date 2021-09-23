35dfd1ea1bfc486dc4d0ab5f2a443b1ab9abb5fe
umarsohail1998 <umarsohail1998@gmail.com>
2021-09-23 22:12:29 +0500

    umar

--- main.py
+++ main.py
@@ -36,6 +36,14 @@ def extract_series_from_web_table(lt):
         Series_dict[S_name] = val
     return Series_dict
 
+def ismatch(s1, s2):
+    length = len(s1)-1
+    start = 0
+    while start<length:
+        if s1[start]!=s2[start]:
+            return False
+        start+=1
+    return True
 
 def Add_serie(driver, name):
     goto = 'http://149.248.20.234:25500/serie.php'
@@ -43,19 +51,28 @@ def Add_serie(driver, name):
     driver.find_element_by_id('title').send_keys(name)
     # driver.implicitly_wait(3)    
     driver.find_element_by_link_text("Next").click()
-    driver.find_element_by_link_text("Previous").click()
-    sleep(1)
-    
-    driver.find_element_by_id('select2-tmdb_search-container').click()        
-    html_list = driver.find_element_by_id("select2-tmdb_search-results")
-    html_list.find_elements_by_tag_name("li")[1].click()
-
     sleep(2)
+    driver.find_element_by_link_text("Previous").click()
+    sleep(2)
+    
+    driver.find_element_by_id('select2-tmdb_search-container').click()
+    sleep(1)        
+    html_list = driver.find_element_by_id("select2-tmdb_search-results")
+    # html_list.find_elements_by_tag_name("li")[1].click()
+    
+    rows  = html_list.find_elements_by_tag_name("li")
+    for idx, row in enumerate(rows):
+        if ismatch(name , row.text):
+            rows[idx+1].click()
+            print(row.text, name)
+            break
+
     driver.find_element_by_xpath('//*[@id="stream-details"]/div/div/div[4]/div/span/span[1]/span/ul').click()
     driver.find_element_by_id('select2-bouquets-results').click()
-    
+    sleep(2)
     driver.find_element_by_link_text("Next").click()
-    driver.implicitly_wait(3)
+    # driver.implicitly_wait(3)
+    sleep(2)
     driver.find_element_by_xpath('//*[@id="movie-information"]/ul/li[2]/input').click()
     
 
@@ -63,19 +80,25 @@ def Add_series_to_web(driver, web, local):
     # print(web, local)
     for x in local:
         if x not in web:
-            Add_serie(driver, x) 
+            Add_serie(driver, x)
+            print(x)
             driver.implicitly_wait(3)
             
 
 def find_All_Series(driver):
     d = {}    
-    lt = driver.find_elements_by_xpath("//*[@id='datatable-streampage']/tbody/tr")
-    d.update(extract_series_from_web_table(lt))
+    page = 2
+    max_pages = 15
+    while page<=max_pages:
+        lt = driver.find_elements_by_xpath("//*[@id='datatable-streampage']/tbody/tr")
+        d.update(extract_series_from_web_table(lt))
+        try:
+            driver.find_element_by_link_text(str(page)).click()
+        except:
+            break
+        sleep(1)
+        page += 1    
     return d
-
-    
-
-
 
 
 
@@ -97,17 +120,24 @@ wait.until(ec.presence_of_element_located((By.XPATH, "//button[@type='submit']")
 
 driver.get("http://149.248.20.234:25500/series.php")
 sleep(2)
-web_Srs_dict = find_All_Series()
+web_Srs_dict = find_All_Series(driver)
+# print(web_Srs_dict)
+
 
 file_record = Extractdata.getData()
-file_Series_name = file_record.keys()
-Add_series_to_web(driver, web_Srs_dict.keys(), file_Series_name)
+# print(len(web_Srs_dict.keys()), len(file_record.keys()))
+# print(web_Srs_dict.keys(), file_record.keys())
 
-driver.get("http://149.248.20.234:25500/series.php")
-driver.close()
+# for x in list(file_record.keys()):
+    # if x not in list(web_Srs_dict.keys()):
+        # print(x)
+Add_series_to_web(driver, list(web_Srs_dict.keys()), list(file_record.keys()))
 
 
-# for x, y in record.items():
+# driver.get("http://149.248.20.234:25500/series.php")
+
+# driver.close()
+# for x, y in file_record.items():
 #     for i in y:
 #         print(x, i['S#'], i['EP#'])
 
