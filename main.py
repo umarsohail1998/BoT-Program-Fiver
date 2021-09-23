8a1e9d38d74a3cba1ddab8c3cf52b01e944d50a8
umarsohail1998 <umarsohail1998@gmail.com>
2021-09-23 11:27:01 +0500

    Add Series done

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
+from time import * 
 from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
@@ -35,29 +38,40 @@ url = "https://srv1.streamsology.net:8443/series/darrellmills/Kv66ULdfPX/22995.m
 
 driver, wait = get_browser()
 driver.get('http://149.248.20.234:25500/login.php')
-
+driver.maximize_window()
 
 wait.until(ec.presence_of_element_located((By.NAME, 'username'))).send_keys(user_name)
 wait.until(ec.presence_of_element_located((By.NAME, 'password'))).send_keys(user_password)
 wait.until(ec.presence_of_element_located((By.XPATH, "//button[@type='submit']"))).click()
-# driver.get("http://149.248.20.234:25500/serie.php")
+
+
+
 driver.get("http://149.248.20.234:25500/episode.php?sid=1")
 
 driver.implicitly_wait(3)
 
 driver.find_element_by_id("season_num").send_keys(s)
 driver.find_element_by_id("episode").send_keys(ep)
+
 driver.find_element_by_id('select2-tmdb_search-container').click()
 
-# issue here.....
-options = driver.find_element_by_id('select2-tmdb_search-results')
-options[len(options)-1].click()
+# tmp = driver.find_element_by_id("select2-tmdb_search-results")
+# for x in tmp:
+#     print(x.get_attribute('innerHTML'))
+
+html_list = driver.find_element_by_id("select2-tmdb_search-results")
+html_list.find_elements_by_tag_name("li")[-1].click()
 
 driver.find_element_by_id("stream_source").send_keys(url)
-driver.find_element_by_name("Next").click()
-driver.find_element_by_name("Next").click()
-driver.find_element_by_name("Next").click()
-driver.find_element_by_name('submit_stream').click()
 
-# driver.find_element_by_name("password").send_keys(user_password)
-# driver.find_element_by_xpath("//button[@type='submit']").click()
+driver.find_element_by_link_text("Next").click()
+driver.find_element_by_link_text("Next").click()
+driver.find_element_by_link_text("Next").click()
+
+source = driver.find_element_by_link_text("Main Server")
+target = driver.find_element_by_link_text("Stream Source")
+
+action_chains = ActionChains(driver)
+action_chains.drag_and_drop(source, target).perform()
+
+driver.find_element_by_name('submit_stream').click()
